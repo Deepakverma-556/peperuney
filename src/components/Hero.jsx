cb7a2786734bb8b62998ed00c12a1f2f08d55690
Deepak Verma <deepakradialcode@gmail.com>
2025-03-05 09:54:05 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import Header from "./common/Header"
+
+function Hero() {
+    return (
+        <div>
+            <Header />
+            <p></p>
+        </div>
+    )
+}
+
+export default Hero
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const config = {
+    plugins: ["@tailwindcss/postcss"],
+    theme: {
+        extend: {
+            screen: {
+                lg: '1025px',
+                md:'770px',
+            }
+        },
+    },
+};
+
+export default config;

8e0673667193127eb808020b974cf3ca25d06184
Deepak Verma <deepakradialcode@gmail.com>
2025-03-05 12:53:09 +0530

    hero responsive

--- src/components/common/CustomButton.jsx
+++ src/components/common/CustomButton.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 function CustomButton({text,myClass}) {
   return (
-      <button className={`${myClass} relative flex w-[262px] h-[75px] max-lg:h-12 max-lg:w-44 max-sm:h-10 max-sm:w-36 cursor-pointer items-center justify-center`}>
+      <button className={`${myClass} relative flex w-[262px] h-[75px] max-lg:h-12 max-lg:w-44 max-sm:h-10 hover:scale-105 transition-all duration-300 max-sm:w-36 cursor-pointer items-center justify-center`}>
           <Image className="absolute top-0 left-0" width={262} height={75} src={"/assets/images/png/button.png"} alt="button" />
           <span className="z-20 ff-luckiest text-[28px] max-lg:text-xl max-sm:text-sm leading-7 text-white">{text}</span>
     </button>
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -8,7 +8,7 @@ import Link from "next/link"
 function Header() {
     // const [open, setOpen] = useState(false)
     return (
-        <div className="max-w-[1332px] mx-auto px-4 flex justify-between items-center pt-[30px] max-sm:pt-4">
+        <div className="max-w-[1332px] mx-auto px-4 flex justify-between items-center pt-[30px] max-sm:pt-5">
             <CustomButton text={"BUY $PEPERUNEY"} />
             <div>
                 {/* <button onClick={() => setOpen(!open)} className="flex cursor-pointer items-center gap-[14px] relative">
@@ -21,7 +21,7 @@ function Header() {
                 </button> */}
                 <div className="flex items-center gap-2">
                     {HEADER_ICONS_LIST.map((obj, i) => (
-                        <div key={i} className="relative h-[67px] w-[77px] max-lg:w-12 max-lg:h-10 max-sm:h-7 max-sm:w-9 flex items-center justify-center">
+                        <div key={i} className="relative h-[67px] w-[77px] max-lg:w-12 max-lg:h-10 max-sm:h-8 max-sm:w-9 flex items-center justify-center">
                             <Image src={'/assets/images/png/icon-bg.png'} alt="icon-bg" height={67} width={77} className="absolute top-0 left-0" />
                             <Link target="blank" href={obj.link} className="z-20">{obj.icon}</Link>
                         </div>

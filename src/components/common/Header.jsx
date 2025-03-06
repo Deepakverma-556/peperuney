35246967e909c6dde074f5f25111007473b10a3f
Deepak Verma <deepakradialcode@gmail.com>
2025-03-06 11:54:45 +0530

    create dropdown

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,19 +6,19 @@ import { HEADER_ICONS_LIST } from "@/utils/helper"
 import Link from "next/link"
 
 function Header() {
-    // const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
     return (
         <div className="max-w-[1332px] mx-auto px-4 flex justify-between items-center pt-[30px] max-sm:pt-5">
             <CustomButton text={"BUY $PEPERUNEY"} />
-            <div>
-                {/* <button onClick={() => setOpen(!open)} className="flex cursor-pointer items-center gap-[14px] relative">
-                    <Image src={'/assets/images/png/flag.png'} alt="flag" height={31} width={41}/>
-                    <div className={`absolute flex flex-col gap-2 transition-all duration-200 ${open ? "opacit-100 top-10" : "opacity-0 top-0"}`}>
-                        <Image src={'/assets/images/png/indian-flag.png'} alt="indian-flag" height={31} width={41} />
-                        <Image src={'/assets/images/png/flag.png'} alt="flag" height={31} width={41} />
+            <div className="flex gap-8 max-md:gap-2">
+                <button onClick={() => setOpen(!open)} className="flex cursor-pointer items-center gap-[14px] max-sm:gap-1 relative">
+                    <Image src={'/assets/images/png/flag.png'} alt="flag" height={31} width={41} className="max-sm:h-6 max-sm:w-8"/>
+                    <div className={`absolute flex flex-col gap-2 transition-all duration-200 ${open ? "opacit-100 top-14 max-sm:top-10" : "opacity-0 top-0"}`}>
+                        <Image src={'/assets/images/png/indian-flag.png'} alt="indian-flag" height={31} width={41} className="max-sm:h-6 max-sm:w-8" />
+                        <Image src={'/assets/images/png/flag.png'} alt="flag" height={31} width={41} className="max-sm:h-6 max-sm:w-8" />
                     </div>
                     <div><DropdownArrow myClass={`${open ? "rotate-180" : ""} transition-all duration-200`} /></div>
-                </button> */}
+                </button>
                 <div className="flex items-center gap-2">
                     {HEADER_ICONS_LIST.map((obj, i) => (
                         <div key={i} className="relative h-[67px] w-[77px] max-lg:w-12 max-lg:h-10 max-sm:h-8 max-sm:w-9 flex items-center justify-center">
